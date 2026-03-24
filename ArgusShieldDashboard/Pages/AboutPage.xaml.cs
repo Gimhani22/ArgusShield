57a5cfe239ff1ecf39d55430e66171356e271264
Gimhani22 <kaveeshagimhanit@gmail.com>
2026-03-24 06:57:21 +0530

    feat: Update About page on the ArgusShield dashboard.

---
+++ ArgusShieldDashboard/Pages/AboutPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ArgusShieldDashboard.Pages
+{
+    public partial class AboutPage : System.Windows.Controls.UserControl
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
